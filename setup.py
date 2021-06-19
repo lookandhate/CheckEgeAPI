8cc425bb1f194e4f9aa8a6aec904daf0ccae63ea
lookandhate <princessselestiapwnz@mail.ru>
2021-06-19 22:02:22 +0400

    Setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CheckEgeAPI',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='https://github.com/lookandhate',
+    author_email='',
+    description='CheckEGE API',
+    
+)
